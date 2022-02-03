68f59ae535643272561db1b072303523e5113140
Todd Gaunt <todd@bastionburrow.com>
2022-02-03 12:34:38 -0500

    Shorten obvious variable names and improve comments

--- vector.go
+++ vector.go
@@ -23,9 +23,10 @@ type Vector struct {
 	tail  []int    // Quickly access elements at the end of the vector
 }
 
-// idx finds the bits of i used to index a node at a given level.
-func idx(index, level int) int {
-	return (index >> (level * nodeBits)) & nodeMask
+// idx extracts the bits from i that are needed to index a node at a given
+// level in the tree.
+func idx(i, level int) int {
+	return (i >> (level * nodeBits)) & nodeMask
 }
 
 // tailOffset returns the total number of elements within a Vector minus the tail.
@@ -33,20 +34,22 @@ func (v *Vector) tailOffset() int {
 	return v.count - len(v.tail)
 }
 
-// isDeepEnough returns true if tree within the vector is deep enough for the
-// amount of elements it contains.
+// isDeepEnough returns true if the tree within the vector is deep enough for
+// another element to be added given the amount of elements it contains.
+// Otherwise false is returned when a new node needs to be added in order to
+// make room.
 func isDeepEnough(length, shift int) bool {
 	return (length >> nodeBits) <= (1 << shift)
 }
 
 // findValues returns the slice of values within the vector which contains the
-// value index is associated with.
-func (v Vector) findValues(index int) []int {
-	if index < 0 || index >= v.count {
+// value i is associated with.
+func (v Vector) findValues(i int) []int {
+	if i < 0 || i >= v.count {
 		panic("index out of bounds")
 	}
 
-	if index >= v.tailOffset() {
+	if i >= v.tailOffset() {
 		return v.tail
 	}
 
@@ -54,7 +57,7 @@ func (v Vector) findValues(index int) []int {
 	// node it is associated with.
 	walk := v.root
 	for level := v.depth; level > 0; level -= 1 {
-		walk = walk.children[idx(index, level)]
+		walk = walk.children[idx(i, level)]
 	}
 
 	return walk.values
@@ -78,10 +81,14 @@ func (v *Vector) String() string {
 	return s
 }
 
+func (v Vector) Count() int {
+	return v.count
+}
+
 // Nth returns from the vector the value at the index provided. The index must
 // be greater than zero and less than v.count.
-func (v Vector) Nth(index int) int {
-	return v.findValues(index)[idx(index, 0)]
+func (v Vector) Nth(i int) int {
+	return v.findValues(i)[idx(i, 0)]
 }
 
 // Peek returns the last value from a vector.
@@ -89,10 +96,10 @@ func (v Vector) Peek() int {
 	return v.Nth(v.count - 1)
 }
 
-// Assoc creates a new vector that contains value at index. The index must be
-// greater than zero and less than v.count.
-func (v Vector) Assoc(index int, value int) Vector {
-	if index < 0 || index >= v.count {
+// Assoc creates a new vector that contains val at the location indexed by key.
+// The key must be greater than zero and less than v.Count().
+func (v Vector) Assoc(key int, val int) Vector {
+	if key < 0 || key >= v.count {
 		panic("index out of bounds")
 	}
 
@@ -101,7 +108,7 @@ func (v Vector) Assoc(index int, value int) Vector {
 
 	var leaf []int
 	// Either the tail is being updated, or a node in the tree is
-	if index >= v.tailOffset() {
+	if key >= v.tailOffset() {
 		// The value to update is in the tail, so make a copy of the tail
 		newTail = make([]int, len(v.tail))
 		copy(newTail, v.tail)
@@ -117,19 +124,19 @@ func (v Vector) Assoc(index int, value int) Vector {
 
 		walk := newRoot
 		for level := v.depth; level > 0; level -= 1 {
-			oldNode := walk.children[idx(index, level)]
+			oldNode := walk.children[idx(key, level)]
 
-			walk.children[idx(index, level)] = &vecNode{}
+			walk.children[idx(key, level)] = &vecNode{}
 			walk.children = append([]*vecNode{}, oldNode.children...)
 			walk.values = append([]int{}, oldNode.values...)
 
-			walk = walk.children[idx(index, level)]
+			walk = walk.children[idx(key, level)]
 		}
 		leaf = walk.values
 	}
 
 	// Update the value
-	leaf[idx(index, 0)] = value
+	leaf[idx(key, 0)] = val
 
 	return Vector{
 		depth: v.depth,
