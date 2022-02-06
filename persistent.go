2c192195b3e7e1282e78d8be081da3cbab37f6aa
Todd Gaunt <todd@bastionburrow.com>
2022-02-06 13:50:44 -0500

    Define beginning collection interface

--- persistent.go
+++ persistent.go
@@ -3,5 +3,16 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 // Package persistent provides data structures for go that that always
-// preserves the previous version of themselves when modified.
+// preserves the previous version of themselves when performing operations on
+// them. Since these structures are immutable, this allows for liberal memory
+// sharing between instances as they are operated upon.
 package persistent
+
+type Collection[T any] interface {
+	Count() int
+	Conj(T) T
+}
+
+func Empty[T any](c Collection[T]) bool {
+	return c.Count() == 0
+}
