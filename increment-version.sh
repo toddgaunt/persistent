6737e042b4c30355e90bf02a2505d816a6c2f302
Todd Gaunt <todd@bastionburrow.com>
2022-10-21 21:57:19 -0400

    persistent: Add version information for v0.1.0

---
+++ increment-version.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+function version() {
+	version=$(cat VERSION.txt)
+	a=( ${version//./ } )
+	major=${a[0]}
+	minor=${a[1]}
+	patch=${a[2]}
+
+	while true; do
+		read -p "$prompt [(M)ajor/m(I)nor/(P)atch]" part
+		if [[ "$part" == "" ]]; then
+			part="M"
+		fi
+		case "$part" in
+			[Mm])
+				major=$((major + 1))
+				minor=0
+				patch=0
+				break
+			;;
+			[Ii])
+				minor=$((minor + 1))
+				patch=0
+				break
+			;;
+			[Pp])
+				patch=$((patch + 1))
+				break
+			;;
+			*)
+				echo "Enter M to increment major version, I for minor version, or P for patch"
+			;;
+		esac
+	done
+
+	version="$major.$minor.$patch"
+	echo "$version"
+	echo "$version" > VERSION.txt
+}
+
+version $@
